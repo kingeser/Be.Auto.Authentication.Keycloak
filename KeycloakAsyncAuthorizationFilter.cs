e444f4bfccc2b2d3456e74de1a03e27be1a4536e
Burak ESER <burak-eser@windowslive.com>
2023-12-01 16:50:32 +0300

    Support Controller Feature
    Support Controller Feature & Improvements

---
+++ KeycloakAsyncAuthorizationFilter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Be.Auto.Authentication.Keycloak.Role;
+
+public class KeycloakAsyncAuthorizationFilter : IAsyncAuthorizationFilter
+{
+
+    public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        if (!context.ActionDescriptor.EndpointMetadata
+                .OfType<AuthorizeAttribute>()
+                .Any())
+        {
+            return Task.CompletedTask;
+        }
+
+        switch (context.ActionDescriptor)
+        {
+            case ControllerActionDescriptor controllerActionDescriptor:
+                {
+                    var controllerName = RoleExtension.Normalize(controllerActionDescriptor.ControllerTypeInfo.Name);
+                    var actionName = controllerActionDescriptor.ActionName;
+                    var controllerRole = $"Controllers.{controllerName}.{actionName}";
+
+                    if (!context.HttpContext.User.IsInRole(controllerRole))
+                    {
+                        context.Result = new UnauthorizedResult();
+                    }
+
+                    break;
+                }
+            case CompiledPageActionDescriptor pageActionDescriptor:
+                {
+                    var pageRole = $"Pages.{RoleExtension.Normalize(pageActionDescriptor.ModelTypeInfo?.Name ?? "")}";
+
+                    if (!context.HttpContext.User.IsInRole(pageRole))
+                    {
+                        context.Result = new UnauthorizedResult();
+                    }
+                    break;
+                }
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ KeycloakBuilderExtensions.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
+namespace Be.Auto.Authentication.Keycloak.Role;
+
+public static class KeycloakBuilderExtensions
+{
+    public static IApplicationBuilder MigrateKeycloakRoles(this IApplicationBuilder builder, Action<KeycloakOption> opt)
+    {
+        if (builder == null)
+        {
+            throw new ArgumentNullException(nameof(builder));
+        }
+
+        if (opt == null)
+        {
+            throw new ArgumentNullException(nameof(opt));
+        }
+
+        var options = new KeycloakOption()
+        {
+            ClientId = string.Empty,
+            AdminRealm = string.Empty,
+            ClientRealm = string.Empty,
+            AdminClientId = string.Empty,
+            AdminClientSecret = string.Empty,
+            KeycloakUrl = string.Empty,
+            AdminScope = string.Empty,
+        
+        };
+
+        opt(options);
+
+        if (string.IsNullOrEmpty(options.KeycloakUrl))
+        {
+            throw new ArgumentNullException(nameof(options.KeycloakUrl));
+        }
+
+        if (string.IsNullOrEmpty(options.AdminRealm))
+        {
+            throw new ArgumentNullException(nameof(options.AdminRealm));
+        }
+
+        if (string.IsNullOrEmpty(options.AdminClientId))
+        {
+            throw new ArgumentNullException(nameof(options.AdminClientId));
+        }
+
+        if (string.IsNullOrEmpty(options.AdminClientSecret))
+        {
+            throw new ArgumentNullException(nameof(options.AdminClientSecret));
+        }
+        if (string.IsNullOrEmpty(options.ClientId))
+        {
+            throw new ArgumentNullException(nameof(options.ClientId));
+        }
+        if (string.IsNullOrEmpty(options.ClientRealm))
+        {
+            throw new ArgumentNullException(nameof(options.ClientRealm));
+        }
+
+        var tool = new KeycloakApplicationRoleMigrationTool(options,builder.ApplicationServices.GetRequiredService<ApplicationPartManager>());
+
+        tool.Migrate();
+
+        return builder;
+    }
+
+
+
+}
